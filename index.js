e3af4ebc29f2e8693324382d02ea6810a98f9bc3
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2022-11-01 16:32:54 +0700

    test

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const hbs = require('hbs'); 
+const path = require('path');
 
 const app = express();
 const PORT = 5000;
@@ -7,6 +9,12 @@ const indexRouter = require('./routes/index-routes.js');
 const mssvRouter = require('./routes/mssv-routes.js');
 const messageRouter = require('./routes/message-routes.js');
 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+// app.use('/', express.static(path.join(__dirname, 'public')));
+
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -14,6 +22,13 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use('/home', (req, res) => {
+    res.render('index',  {
+        title: "Home",
+        content: "Hello from index",
+        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia1.tenor.com%2Fimages%2F100791a048be5d7bab568720fba1f79c%2Ftenor.gif%3Fitemid%3D16605201&f=1&nofb=1&ipt=b1f11bc8bfb45a09ae7ba673510f47c32713d00f61595027acf7abd05851f54e&ipo=images"
+    });
+});
 app.use('/', indexRouter);
 app.use('/19110426', mssvRouter);
 app.use('/message', messageRouter);
